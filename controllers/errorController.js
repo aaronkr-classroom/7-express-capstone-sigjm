229bbea3940817355365e08407dd9e37a01e375d
jmllem <sigheartjm@gmail.com>
2024-04-20 18:10:53 +0900

    12

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,8 +1,6 @@
 // errorController.js
 "use strict";
 
-
-
 /**
  * Listing 11.2 (p. 168)
  */
@@ -12,7 +10,21 @@
  * Listing 12.11 (p. 184)
  * 에러 처리 라우트 추가
  */
+const httpStatus = require("http-status-codes");
 
+exports.pageNotFoundError = (req, res) => {
+    let errorCode = httpStatus.NOT_FOUND;
+    res.status(errorCode);
+    res.render("error");
+};
+
+exports.resInternalError = (err, req, res, next) => {
+    let errorCode = httpStatus.INTERNAL_SERVER_ERROR;
+    //console,log(`ERROR occurred: ${error.stack}`);
+    res.status(errorCode);
+    //res.send(`${errorCode} | Sorry, our application is taking a nap!`);
+    res.render("error");
+};
 
 /**
  * [노트] 라우트 순서는 중요하다. 이 라우트는 기존에 존재하는 라우트 아래에 와야 한다.
