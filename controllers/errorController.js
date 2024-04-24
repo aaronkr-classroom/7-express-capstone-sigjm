a142dbdbec77d0ae7fb83bc96eaca6eee125a9eb
jmllem <sigheartjm@gmail.com>
2024-04-24 12:42:07 +0900

    12

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -28,6 +28,8 @@ exports.resInternalError = (err, req, res, next) => { // 내부 서버 에러의
     res.send(`${errorCode} | Sorry, our app is experiencing a problem!`);
 }
 
+
+
 /**
  * [노트] 라우트 순서는 중요하다. 이 라우트는 기존에 존재하는 라우트 아래에 와야 한다.
  * 기존 라우트는 범용으로 사용되는 것이며 하위 라우트에 오버라이딩되기 때문이다.
