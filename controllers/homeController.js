6b994bc1666747b5047c9a4f67aa5b565771fd84
jmllem <sigheartjm@gmail.com>
2024-04-24 12:54:57 +0900

    12test333

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -3,12 +3,12 @@
 // "use strict";
 /**
  * Listing 12.5 (p. 178)
- * 홈 컨트롤러로의 라우팅
+ * 홈 컨트롤러로의 라우팅 check
  */
 exports.showHome = (req, res) => {
     res.render("index");
 }
-exports.showTransportation = (req, res) => { // 특정 라우트를 위한 콜백 함수 추가
+exports.showTransportation = (req, res)  => { // 특정 라우트를 위한 콜백 함수 추가
     res.render("transportation");
 };
 exports.showSignUp = (req, res) => {
